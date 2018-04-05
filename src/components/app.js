cdb37085cde29d66df6e03a3853ec711ff781197
Autumn <autumncbullard@gmail.com>
2018-04-05 10:43:57 -0400

    Add markup for footer

--- src/components/app.js
+++ src/components/app.js
@@ -8,6 +8,7 @@ export default class App extends Component {
         <header className="text-left">
           <h1>Wikipedia Viewer</h1>
         </header>
+        <footer className="text-center">Coded by <a href="../portfolio" target="_blank">Autumn Bullard</a></footer>
       </div>
     );
   }
