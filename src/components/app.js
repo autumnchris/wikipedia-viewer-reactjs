03864024b9d6b473cbb58df6e5da7ae887747be3
Autumn <autumncbullard@gmail.com>
2018-04-05 10:42:10 -0400

    Add markup for header

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,11 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="container-fluid"></div>
+      <div className="container-fluid">
+        <header className="text-left">
+          <h1>Wikipedia Viewer</h1>
+        </header>
+      </div>
     );
   }
 }
