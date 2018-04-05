b04742393bf69c94c552be55d3498b03169f2f2e
Autumn <autumncbullard@gmail.com>
2018-04-05 12:20:24 -0400

    Make a GET request to the Media Wiki API whenever the form is submitted

--- src/components/app.js
+++ src/components/app.js
@@ -9,6 +9,23 @@ export default class App extends Component {
       searchInput: '',
       searchResults: []
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      searchInput: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    axios.get(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${this.state.searchInput}&origin=*&format=json`)
+    .then((wikiData) => {
+      this.setState({ searchResults: wikiData.data.query.search });
+      console.log(wikiData);
+    });
   }
 
   render() {
@@ -21,10 +38,10 @@ export default class App extends Component {
           <div className="text-center">
             <span className="fa fa-wikipedia-w fa-4x"></span>
           </div>
-          <form className="center-block" role="search">
+          <form className="center-block" role="search" onSubmit={this.handleSubmit}>
             <div className="form-group has-feedback">
               <span className="fa fa-search form-control-feedback"></span>
-              <input type="search" className="form-control search-input" aria-label="Search for a Wikipedia article..." placeholder="Search for a Wikipedia article..." required />
+              <input type="search" className="form-control search-input" aria-label="Search for a Wikipedia article..." placeholder="Search for a Wikipedia article..." onChange={this.handleChange} value={this.state.searchInput} required />
             </div>
           </form>
           <p className="text-center">...or read a <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank">random Wikipedia article</a></p>
