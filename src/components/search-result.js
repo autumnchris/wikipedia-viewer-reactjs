ffab309047463fecf4f939a7b42b56fd85a0c03f
Autumn <autumncbullard@gmail.com>
2018-04-07 22:12:06 -0400

    Fix links to searched articles

--- src/components/search-result.js
+++ src/components/search-result.js
@@ -7,7 +7,7 @@ const SearchResult = ({ article }) => {
       <div className="well center-block">
         <h2>{article.title}</h2>
         <p dangerouslySetInnerHTML={{__html: `${article.snippet}...`}}></p>
-        <a href="`https://en.wikipedia.org/wiki/${article.title}`" target="_blank">Continue Reading...</a>
+        <a href={`https://en.wikipedia.org/wiki/${article.title}`} target="_blank">Continue Reading...</a>
       </div>
     </article>
   );
