c03128d51b393c0d54f2b35f4d8cd221c27e3d7f
Autumn <autumncbullard@gmail.com>
2018-12-08 15:35:31 -0500

    Use @import to organize Sass stylesheets

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
-require('./stylesheets/reset.scss');
 require('./stylesheets/style.scss');
 
 ReactDOM.render(<App />, document.getElementById('app'));
