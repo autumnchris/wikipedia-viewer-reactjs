807a235c41ec8bec0bda980aff16648e80461397
autumnchris <autumncbullard@gmail.com>
2021-07-21 15:50:17 -0400

    Use React Hooks instead of class components

--- src/components/app.js
+++ src/components/app.js
@@ -1,85 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchForm from './search-form';
 import LoadingSpinner from './loading-spinner';
 import ResultsContainer from './results-container';
 import axios from 'axios';
 
-class App extends React.Component {
+const App = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [loadingStatus, setLoadingStatus] = useState(false);
+  const [searchError, setSearchError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchInput: '',
-      searchResults: [],
-      isLoading: false,
-      searchError: false,
-      errorMessage: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  function handleChange(event) {
+    setSearchInput(event.target.value);
   }
 
-  handleChange(event) {
-    this.setState({
-      searchInput: event.target.value
-    });
-  }
-
-  handleSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    this.setState({
-      searchResults: [],
-      isLoading: true
-    });
+    setSearchResults([]);
+    setLoadingStatus(true);
 
-    if (!this.state.searchInput) {
-      this.setState({
-        isLoading: false,
-        searchError: true,
-        errorMessage: 'A text input must be submitted to get search results.'
-      });
+    if (!searchInput) {
+      setLoadingStatus(false);
+      setSearchError(true);
+      setErrorMessage('A text input must be submitted to get search results.');
     }
     else {
-      axios.get(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${this.state.searchInput}&origin=*&format=json`)
-      .then(wikiData => {
-        this.setState({
-          searchResults: wikiData.data.query.search,
-          isLoading: false,
-          searchError: false,
-          errorMessage: ''
-        });
-
-        if (this.state.searchResults.length === 0) {
-          this.setState({
-            searchError: true,
-            errorMessage: `Unable to find results for \"${this.state.searchInput}\". Consider revising your search.`
-          });
-        }
-      }).catch(error => {
-        this.setState({
-          isLoading: false,
-          searchError: true,
-          errorMessage: 'Unable to load Wikipedia search results at this time.'
-        });
-      });
+      fetchSearchResults();
     }
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <header>
-          <h1>Wikipedia Viewer</h1>
-        </header>
-        <main>
-          <div className="fab fa-wikipedia-w fa-4x"></div>
-          <SearchForm searchInput={this.state.searchInput} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
-          {this.state.isLoading ? <LoadingSpinner /> : <ResultsContainer searchResults={this.state.searchResults} searchError={this.state.searchError} errorMessage={this.state.errorMessage} />}
-        </main>
-        <footer>Created by <a href="https://autumnbullard-portfolio.herokuapp.com" target="_blank">Autumn Bullard</a> &copy; {new Date().getFullYear()}</footer>
-      </React.Fragment>
-    );
+  function fetchSearchResults() {
+    axios.get(`https://en.wikipedia.org/w/api.php?action=query&list=se4arch&srsearch=${searchInput}&origin=*&format=json`)
+    .then(response => {
+      setLoadingStatus(false);
+      renderSearchResults(response.data.query.search);
+    }).catch(error => {
+      setLoadingStatus(false);
+      setSearchError(true);
+      setErrorMessage('Unable to load Wikipedia search results at this time.');
+    });
   }
+
+  function renderSearchResults(results) {
+
+    if (results.length !== 0) {
+      setSearchResults(results);
+      setSearchError(false);
+      setErrorMessage('');
+    }
+    else {
+      setSearchError(true);
+      setErrorMessage(`Unable to find results for \"${searchInput}\". Consider revising your search.`);
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <header>
+        <h1>Wikipedia Viewer</h1>
+      </header>
+      <main>
+        <div className="fab fa-wikipedia-w fa-4x"></div>
+        <SearchForm searchInput={searchInput} handleChange={handleChange} handleSubmit={handleSubmit} />
+        {loadingStatus ? <LoadingSpinner /> : <ResultsContainer searchResults={searchResults} searchError={searchError} errorMessage={errorMessage} />}
+      </main>
+      <footer>Created by <a href="https://autumnbullard-portfolio.herokuapp.com" target="_blank">Autumn Bullard</a> &copy; {new Date().getFullYear()}</footer>
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/components/results-container.js
+++ src/components/results-container.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ErrorMessage from './error-message';
 import ResultItem from './result-item';
 
 const ResultsContainer = ({ searchResults, searchError, errorMessage }) => {
 
   if (searchError) {
-    return <p className="message error-message"><span className="fa fa-exclamation-circle fa-lg fa-fw"></span> {errorMessage}</p>;
+    return <ErrorMessage errorMessage={errorMessage} />;
   }
   else {
     return <div className="search-results">{searchResults.map(resultItem => <ResultItem key={resultItem.pageid} resultItem={resultItem} />)}</div>;
