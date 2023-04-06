bc5c522ad9c91c8b4b1fe5bb32a3f25214656ad1
autumnchris <autumncbullard@gmail.com>
2023-04-06 13:28:21 -0400

    Fix typo

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -52,7 +52,7 @@ const SearchForm = ({ setLoadingStatus, setErrorMessage, setSearchResults }) => 
         <input type="text" className="search-input" aria-label="Search Wikipedia..." placeholder="Search Wikipedia..." onChange={(event) => handleChange(event)} value={searchInput} required autoFocus />
       </div>
       <div className="button-group">
-        <button tyoe="submit" className="button">Search</button>
+        <button type="submit" className="button">Search</button>
         <a href="https://en.wikipedia.org/wiki/Special:Random" className="button" target="_blank">Random Article</a>
       </div>
     </form>
